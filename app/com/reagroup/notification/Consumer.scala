da7ab46e6473b3141d92d494bc37bd38ec6e532c
Joel Robotham <joel.robotham@rea-group.com>
2013-07-04 12:29:47 +1000

    some initial consumer routes

--- app/com/reagroup/notification/Consumer.scala
+++ app/com/reagroup/notification/Consumer.scala
@@ -2,7 +2,32 @@ package com.reagroup.notification
 
 import play.api.mvc.Controller
 import play.api.mvc.Action
+import play.api.libs.json.Json
+import play.api.libs.json.JsNull
 
 object Consumer extends Controller {
+  def details(consumerId: String) = Action { request =>
+    val results = Map(
+      "count" -> Json.toJson(request.uri + "/count"),
+      "recent" -> Json.toJson(request.uri + "/recent"))
+    Ok(Json.toJson(results))
+  }
+  
   def count(consumerId: String) = Action(Ok(consumerId))
+
+  def recent(consumerId: String) = Action { request =>
+    val results = Map(
+      "users" -> Seq(
+        Json.toJson(
+          Map(
+            "name" -> Json.toJson("Bob"),
+            "age" -> Json.toJson(31),
+            "email" ->Json.toJson("bob@gmail.com"))),
+        Json.toJson(
+          Map(
+            "name" -> Json.toJson("Kiki"),
+            "age" -> Json.toJson(25),
+            "email" -> JsNull))))
+    Ok(Json.toJson(results))
+  }
 }
