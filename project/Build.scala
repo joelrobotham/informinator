32c9868a4f471a3bc2e89c3ab649276bdfe3faea
Supriya Joshi <supriya.joshi@rea-group.com>
2013-07-04 15:56:26 +1000

    Removed the routeImport and templateImport.

--- project/Build.scala
+++ project/Build.scala
@@ -10,18 +10,12 @@ object ApplicationBuild extends Build {
       "mysql" % "mysql-connector-java" % "5.1.6",
       "com.github.nscala-time" %% "nscala-time" % "0.4.2",
       "org.mongodb" %% "casbah" % "2.6.2",
-      "com.novus" %% "salat" % "1.9.2-SNAPSHOT")
+     "com.novus" %% "salat" % "1.9.2-SNAPSHOT")
  
   val main = play.Project(
-    appName, appVersion, appDependencies).settings(
-	    routesImport += "se.radley.plugin.salat.Binders._",
-	    templatesImport += "org.bson.types.ObjectId",
+    appName, appVersion, appDependencies)
+  .settings(
 	    resolvers += Resolver.sonatypeRepo("snapshots")
     )
  
-  /*   val main = play.Project(appName, appVersion, appDependencies).settings(
-    routesImport += "se.radley.plugin.salat.Binders._",
-    templatesImport += "org.bson.types.ObjectId",
-      resolvers += Resolver.sonatypeRepo("snapshots")
-     )*/
 }
