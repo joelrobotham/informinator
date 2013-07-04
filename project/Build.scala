b8da87b5b8f653224442a003d2354fa57d845bb0
Supriya Joshi <supriya.joshi@rea-group.com>
2013-07-04 17:20:22 +1000

    Added the leon/play plugin for salat.

--- project/Build.scala
+++ project/Build.scala
@@ -10,10 +10,13 @@ object ApplicationBuild extends Build {
       "mysql" % "mysql-connector-java" % "5.1.6",
       "com.github.nscala-time" %% "nscala-time" % "0.4.2",
       "org.mongodb" %% "casbah" % "2.6.2",
-     "com.novus" %% "salat" % "1.9.2-SNAPSHOT")
+     "com.novus" %% "salat" % "1.9.2-SNAPSHOT",
+      "se.radley" %% "play-plugins-salat" % "1.2")
  
   val main = play.Project(
     appName, appVersion, appDependencies).settings(
-	    resolvers += Resolver.sonatypeRepo("snapshots")
+	    resolvers += Resolver.sonatypeRepo("snapshots"),
+      routesImport += "se.radley.plugin.salat.Binders._",
+      templatesImport += "org.bson.types.ObjectId"  
     )
 }
