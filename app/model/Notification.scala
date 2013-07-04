308103b74a72a67c8e879fb07ed437fc9b865bc1
Supriya Joshi <supriya.joshi@rea-group.com>
2013-07-04 17:34:54 +1000

    Fixed the connection between salat and play framework.

--- app/model/Notification.scala
+++ app/model/Notification.scala
@@ -1,14 +1,20 @@
 package model
+
+import play.api.Play.current
 import com.novus.salat._
 import com.novus.salat.global._
 import com.novus.salat.annotations._
 import com.novus.salat.dao._
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.MongoConnection
+import se.radley.plugin.salat._
+import salatcontext._
 
 case class Notification(@Key("notificationId") id: String, message: String)
 
-object NotificationDAO extends SalatDAO[Notification, String] (collection = MongoConnection() ("notification_db")("notification_coll"))
+object NotificationDAO extends SalatDAO[Notification, String] (collection = mongoCollection("notification_coll"))
+
+
 
 
 
