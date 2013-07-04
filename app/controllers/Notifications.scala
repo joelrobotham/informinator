feb2523533f57d5e1a57e98b89a6dfca0b1499cc
Steven Bearzatto <steven.bearzatto@rea-group.com>
2013-07-04 17:26:12 +1000

    Added notifications view

--- app/controllers/Notifications.scala
+++ app/controllers/Notifications.scala
@@ -5,6 +5,8 @@ import play.api.libs.json.JsError
 import play.api.libs.json.Json
 import model.Notification
 import model.NotificationDAO
+import views.html
+
 /**
  * Created with IntelliJ IDEA.
  * User: steven_bearzatto
@@ -18,5 +20,10 @@ object Notifications extends Controller {
     val notification = new Notification(Json.stringify(request.body\"email"), Json.stringify(request.body\"message"))
     val id = NotificationDAO.insert(notification)
     Ok("test")
+  }
+
+  def index(email: String) = Action {
+    val notifications = Array(new Notification("safdjk", "A message"), new Notification("safdjk", "2 message"))
+    Ok(html.notifications.index(email, notifications))
   }
 }
