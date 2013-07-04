14a06937d393b4adb2cae988b71cbcafec563189
Joel Robotham <joel.robotham@rea-group.com>
2013-07-04 13:31:18 +1000

    add in joda time

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,8 @@ object ApplicationBuild extends Build {
   val appName         = "MyAccount"
   val appVersion      = "0.1"
   val appDependencies = List(
-      "mysql" % "mysql-connector-java" % "5.1.6")
+      "mysql" % "mysql-connector-java" % "5.1.6",
+      "com.github.nscala-time" %% "nscala-time" % "0.4.2")
  
   val main = play.Project(
     appName, appVersion, appDependencies) 
