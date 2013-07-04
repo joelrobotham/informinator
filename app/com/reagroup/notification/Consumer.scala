8829970146b09058f2cd3163bd9f2a9ed8e38f90
Joel Robotham <joel.robotham@rea-group.com>
2013-07-04 13:42:26 +1000

    add in some example json content

--- app/com/reagroup/notification/Consumer.scala
+++ app/com/reagroup/notification/Consumer.scala
@@ -4,6 +4,7 @@ import play.api.mvc.Controller
 import play.api.mvc.Action
 import play.api.libs.json.Json
 import play.api.libs.json.JsNull
+import org.joda.time.DateTime
 
 object Consumer extends Controller {
   def details(consumerId: String) = Action { request =>
@@ -16,18 +17,24 @@ object Consumer extends Controller {
   def count(consumerId: String) = Action(Ok(consumerId))
 
   def recent(consumerId: String) = Action { request =>
-    val results = Map(
-      "users" -> Seq(
+    val results = Seq(
         Json.toJson(
           Map(
-            "name" -> Json.toJson("Bob"),
-            "age" -> Json.toJson(31),
-            "email" ->Json.toJson("bob@gmail.com"))),
+            "creation" -> Json.toJson(DateTime.now.toString("YYYYMMdd HH:mmZ")),
+            "expiry" -> Json.toJson(DateTime.now.toString("YYYYMMdd HH:mmZ")),
+            "acknowledged" -> Json.toJson(true),
+            "type" -> Json.toJson("PROPERTY_CHANGED"),
+            "message" -> Json.toJson("Listing has been updated"),
+            "body" -> Json.toJson("Some opaque structure that might differ depending on the message type"))),
         Json.toJson(
           Map(
-            "name" -> Json.toJson("Kiki"),
-            "age" -> Json.toJson(25),
-            "email" -> JsNull))))
+            "creation" -> Json.toJson(DateTime.now.minusDays(2).toString("YYYYMMdd HH:mmZ")),
+            "expiry" -> Json.toJson(DateTime.now.plusDays(1).toString("YYYYMMdd HH:mmZ")),
+            "acknowledged" -> Json.toJson(false),
+            "type" -> Json.toJson("PROPERTY_CHANGED"),
+            "message" -> Json.toJson("Listing has been updated"),
+            "body" -> Json.toJson("Some opaque structure")))
+            )
     Ok(Json.toJson(results))
   }
 }
