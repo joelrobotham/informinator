fd2a6e464cc99c1033f3e2fdca6b6fddeab3ea7a
Supriya Joshi <supriya.joshi@rea-group.com>
2013-07-05 13:42:27 +1000

    Sorting by date

--- app/model/Notification.scala
+++ app/model/Notification.scala
@@ -23,8 +23,7 @@ case class Notification(_id: ObjectId = new ObjectId,
   msgType: String)
 
 object NotificationDAO extends SalatDAO[Notification, String](collection = mongoCollection("notification_coll")) {
-
-  def findByEmail(email: String) = find(MongoDBObject("email"->email, "acknowledged" -> false)).toList
+  def findByEmail(email: String) = find(MongoDBObject("email"->email, "acknowledged" -> false)).sort(orderBy = MongoDBObject("creation" -> -1)).toList
   def acknowledge(id: String) {
     val notification = findOne(MongoDBObject("_id"->new ObjectId(id)))
     val updatedNotification = new Notification(_id=notification.get._id, message=notification.get.message,
@@ -38,13 +37,13 @@ object NotificationDAO extends SalatDAO[Notification, String](collection = mongo
 
     update(MongoDBObject("_id"-> new ObjectId(id)), updatedNotification, false, false, new WriteConcern)
   }
-  def findAllByEmail(email: String) = find(MongoDBObject("email"->email)).toList
   def findByDistinctMsgType(messageType: String) = collection.distinct("msgType").toList
   def findByDistinctEmailByMsgType(messageType: String): List[String] = {
     val anyList = collection.distinct("email", MongoDBObject("msgType" -> messageType)).toList
     anyList.map(_.asInstanceOf[String])
   }
 
+  def findAllByEmail(email: String) = find(MongoDBObject("email"->email)).sort(orderBy = MongoDBObject("creation" -> -1)).toList
 }
 
 
