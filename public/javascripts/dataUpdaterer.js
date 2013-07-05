f9000975b3afdc8e13ced2185d6d5b30c960c311
Steven Bearzatto <steven.bearzatto@rea-group.com>
2013-07-05 10:02:16 +1000

    Added view link

--- public/javascripts/dataUpdaterer.js
+++ public/javascripts/dataUpdaterer.js
@@ -4,9 +4,12 @@ function updateData() {
         for(var i in data) {
             html += "<tr><td>A Date</td>";
             html += "<td>" + data[i].message + "</td>";
+            html += '<td><a href="' + data[i].url + '" target="_blank">View</a></td>';
             html += "</tr>";
         }
         $('tbody').html(html);
+
+        $('title').html("(" + data.length + ") Notifications for " + window.user);
     });
 }
 
