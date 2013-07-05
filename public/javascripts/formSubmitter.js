6433adc35c49df35adcef095b2ba09600e9490ce
Steven Bearzatto <steven.bearzatto@rea-group.com>
2013-07-05 13:16:25 +1000

    Added random property generator to for submitinator

--- public/javascripts/formSubmitter.js
+++ public/javascripts/formSubmitter.js
@@ -1,3 +1,9 @@
+window.sampleproperties = [{id: "113998259", url:"http://www.realestate.com.au/property-apartment-vic-richmond-113998259"},
+    {id: "114122019", url:"http://www.realestate.com.au/property-house-vic-richmond-114122019"},
+    {id: "114109519", url:"http://www.realestate.com.au/property-townhouse-vic-richmond-114109519"},
+    {id: "114237143", url:"http://www.realestate.com.au/property-townhouse-vic-richmond-114237143"},
+    {id: "114228491", url:"http://www.realestate.com.au/property-house-vic-richmond-114228491"}];
+
 $('#form').submit(function(event) {
     event.preventDefault();
     var form = this,
@@ -13,5 +19,10 @@ $('#form').submit(function(event) {
             data: JSON.stringify(formSubmitData)})
         .done(function() {
             $("h2").html("Notification Send")
+            var randomPropNo = Math.floor((Math.random()*6));
+            var prop = window.sampleproperties[randomPropNo];
+            $("input[name='message']").val("New property " + prop.id);
+            $("input[name='url']").val(prop.url);
         });
 });
+
